caf41d829b963cdc02eae6ec84ba349c6c721b95
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 12:19:11 +0530

    Simple ListView and DetailView implementation

--- todos_app/views.py
+++ todos_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import UserTask
 
-# Create your views here.
+
+class TaskList(ListView):
+    model = UserTask
+    template_name = 'task_list.html'
+    
+    
+class TaskDetail(DetailView):
+    model = UserTask
+    template_name = 'task_detail.html'
+    
+    
+
+
