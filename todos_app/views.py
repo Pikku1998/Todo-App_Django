8e62f4a07ca99e4bc606b6466490ee76d3b45f9e
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 21:38:09 +0530

    Implemented Search Functionality

--- todos_app/views.py
+++ todos_app/views.py
@@ -45,6 +45,12 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasklist'] = context['tasklist'].filter(user=self.request.user)
+        
+        # Search Functionality in tasks list
+        search_tasks = self.request.GET.get('search_tasks')
+        if search_tasks:
+            context['tasklist'] = context['tasklist'].filter(task__icontains=search_tasks)
+            context['search_tasks'] = search_tasks
         return context
     
     
