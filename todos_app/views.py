45ed83025deab5972e4d8792d601f0b52b76607f
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 12:23:04 +0530

    change default object name

--- todos_app/views.py
+++ todos_app/views.py
@@ -6,11 +6,13 @@ from .models import UserTask
 class TaskList(ListView):
     model = UserTask
     template_name = 'task_list.html'
+    context_object_name = 'tasklist'
     
     
 class TaskDetail(DetailView):
     model = UserTask
     template_name = 'task_detail.html'
+    context_object_name = 'taskdetail'
     
     
 
