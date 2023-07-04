6a2da16bf9db1ea78926ca2247eb8b499e7d4c93
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 16:21:26 +0530

    get user specific data only

--- todos_app/views.py
+++ todos_app/views.py
@@ -18,12 +18,22 @@ class TaskList(LoginRequiredMixin, ListView):
     model = UserTask
     template_name = 'task_list.html'
     context_object_name = 'tasklist'
+    # default name for object is object_list, we override the name by using context_object_name attribute.
+    
+    # By default, context['tasklist'] has all the items/rows of the db. We override this method to get data related to the user only.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasklist'] = context['tasklist'].filter(user=self.request.user)
+        return context
+    
     
     
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = UserTask
     template_name = 'task_detail.html'
     context_object_name = 'taskdetail'
+    # default name for object is object, we override the name by using context_object_name attribute.
+    
     
     
 class CreateTask(LoginRequiredMixin, CreateView):
