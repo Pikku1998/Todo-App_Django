7fa177a80e8ec2c678972aeef85e8c7ec93efc19
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 11:35:31 +0530

    Create a model and register it in admin site

--- todos_app/models.py
+++ todos_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserTask(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    task = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.task
+    
+    class Meta:
+        db_table = 'user_tasks'
+        ordering = ['is_completed']
+    
+
