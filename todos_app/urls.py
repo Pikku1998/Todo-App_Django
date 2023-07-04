0e8915243c8d94dfdc14d4c522706569f4846ce1
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-04 19:56:54 +0530

    Register page implementation with FormView

--- todos_app/urls.py
+++ todos_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, CreateTask, UpdateTask, DeleteTask, Login
+from .views import TaskList, TaskDetail, CreateTask, UpdateTask, DeleteTask, Login, Register
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
+    path('register', Register.as_view(), name='register'),
     path('login', Login.as_view(), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task_detail'),
--- todos_app/views.py
+++ todos_app/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import UserTask
 from django.urls import reverse_lazy
 
+class Register(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('task_list')
+    
+    # save the form and login the user.
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+    
+    # If user is authenticated, restrict him from accessing Register page.
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task_list')
+        return super(Register, self).get(*args, **kwargs)
+        
+    
 
 class Login(LoginView):
     template_name = 'login.html'
